06388849e9e2a7a7041eba6616406eb967a9bbac
Oliver Lok Trevor <featherfeet5436@gmail.com>
2020-02-02 18:09:19 -0800

    Worked on klass editor.

--- Server/static/scripts/klass_editor.js
+++ Server/static/scripts/klass_editor.js
@@ -36,12 +36,6 @@ class Seating {
     containsPoint(x, y) {
         // Returns true if the point is within half of a diagonal of the center of the desk. This makes grabbing rotated desks easier.
         var desk_diagonal = Math.hypot(this.desk_width, this.desk_height);
-        console.log(`desk_diagonal: ${desk_diagonal}`);
-        console.log(`x: ${x}`);
-        console.log(`y: ${y}`);
-        console.log(`this.desk_x: ${this.desk_x}`);
-        console.log(`this.desk_y: ${this.desk_y}`);
-        console.log(`distance(x, y, this.desk_x, this.desk_y): ${distance(x, y, this.desk_x, this.desk_y)}`);
         return distance(x, y, this.desk_x + this.desk_width / 2.0, this.desk_y + this.desk_height / 2.0) <= desk_diagonal / 2.0;
     }
 }
@@ -49,12 +43,15 @@ class Seating {
 class KlassEditor {
     initializeCanvasContextWithDPI(width, height) {
         var devicePixelRatio = window.devicePixelRatio || 1;
+        this.devicePixelRatio = devicePixelRatio;
+        console.log(`this.devicePixelRatio: ${this.devicePixelRatio}`);
         this.canvas_element.width = width * devicePixelRatio;
         this.canvas_element.height = height * devicePixelRatio;
         this.canvas_element.style.width = `${width}px`;
         this.canvas_element.style.height = `${height}px`;
         this.ctx = this.canvas_element.getContext("2d");
         this.ctx.scale(devicePixelRatio, devicePixelRatio);
+        this.redraw();
     }
 
     handleDragStart(event) {
@@ -63,9 +60,7 @@ class KlassEditor {
         for (var i = 0; i < self.seatings.length; i++) {
             var seating = self.seatings[i];
             if (seating.containsPoint(event.offsetX, event.offsetY)) {
-                self.selected_seating = i;
-                self.selected_seating_offset_x = event.offsetX - seating.desk_x;
-                self.selected_seating_offset_y = event.offsetY - seating.desk_y;
+                self.grabbed_seating = i;
                 break;
             }
         }
@@ -74,18 +69,17 @@ class KlassEditor {
     handleDrag(event) {
         var self = event.data;
 
-        if (self.selected_seating != -1) {
-            self.seatings[self.selected_seating].desk_x = event.offsetX - self.selected_seating_offset_x;
-            self.seatings[self.selected_seating].desk_y = event.offsetY - self.selected_seating_offset_y;
+        if (self.grabbed_seating != -1) {
+            self.seatings[self.grabbed_seating].desk_x = event.offsetX - self.grabbed_seating_offset_x;
+            self.seatings[self.grabbed_seating].desk_y = event.offsetY - self.grabbed_seating_offset_y;
+            self.redraw();
         }
-
-        self.redraw();
     }
 
     handleDragEnd(event) {
         var self = event.data;
 
-        self.selected_seating = -1;
+        self.grabbed_seating = -1;
     }
 
     initializeCanvasHandlers() {
@@ -95,13 +89,21 @@ class KlassEditor {
     }
 
     constructor(canvas_element) {
-        this.selected_seating = -1;
-        this.selected_seating_offset_x = 0;
-        this.selected_seating_offset_y = 0;
-        this.canvas_element = canvas_element;
-        this.initializeCanvasContextWithDPI(800, 800);
-        this.initializeCanvasHandlers();
+        // Array of all seatings (Seating objects) being displayed.
         this.seatings = new Array();
+        // The index (in the this.seatings array) of the seating currently being drag-and-dropped.
+        this.grabbed_seating = -1;
+        // The offset distance in pixels of the mouse when it grabbed a seating. This is used so that the seating does not "jump" locations  to align its center with the mouse.
+        this.grabbed_seating_offset_x = 0;
+        this.grabbed_seating_offset_y = 0;
+        // The HTML5 <canvas> element.
+        this.canvas_element = canvas_element;
+        // Used to calculate clicks and scalings for HiDPI displays.
+        this.devicePixelRatio = 1.0;
+        // Initialize the canvas for HiDPI displays.
+        this.initializeCanvasContextWithDPI(800, 800);
+        // Initialize the mouse interaction handlers for the canvas.
+        this.initializeCanvasHandlers();
     }
 
     addSeating(seating) {
@@ -110,6 +112,7 @@ class KlassEditor {
     }
 
     redraw() {
+        console.log("Redrawing...");
         // Clear the canvas.
         this.ctx.clearRect(0, 0, this.canvas_element.width, this.canvas_element.height);
 
@@ -117,6 +120,7 @@ class KlassEditor {
         this.ctx.strokeStyle = "#3b5998";
 
         for (var i = 0; i < this.seatings.length; i++) {
+            this.ctx.save();
             var seating = this.seatings[i];
             var seating_center_x = seating.desk_x + seating.desk_width / 2.0;
             var seating_center_y = seating.desk_y + seating.desk_height / 2.0;
@@ -124,7 +128,8 @@ class KlassEditor {
             this.ctx.rotate(seating.desk_angle);
             this.ctx.translate(-seating_center_x, -seating_center_y);
             this.ctx.strokeRect(seating.desk_x, seating.desk_y, seating.desk_width, seating.desk_height);
-            this.ctx.setTransform(1, 0, 0, 1, 0, 0);
+            //this.ctx.setTransform(1, 0, 0, 1, 0, 0);
+            this.ctx.restore();
         }
     }
 }
